3b7f4f33f9ce8268f2a95aeff2614688b34be518
joeloverbeck <joeloverbeck@gmail.com>
2024-10-23 06:38:33 +0200

    Preparing to implement Instructor-based prompting.

--- src/prompting/factories/openrouter_llm_client_factory.py
+++ src/prompting/factories/openrouter_llm_client_factory.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from openai import OpenAI
+
 from src.base.constants import OPENROUTER_API_URL
 from src.filesystem.filesystem_manager import FilesystemManager
 from src.prompting.abstracts.abstract_factories import LlmClientFactory
@@ -7,8 +10,13 @@ from src.prompting.open_ai_llm_client import OpenAiLlmClient
 
 
 class OpenRouterLlmClientFactory(LlmClientFactory):
+    def __init__(self, filesystem_manager: Optional[FilesystemManager] = None):
+        self._filesystem_manager = filesystem_manager
 
     def create_llm_client(self) -> LlmClient:
-        filesystem_manager = FilesystemManager()
-        return OpenAiLlmClient(OpenAI(base_url=OPENROUTER_API_URL, api_key=
-        filesystem_manager.load_openrouter_secret_key()))
+        return OpenAiLlmClient(
+            OpenAI(
+                api_key=self._filesystem_manager.load_openrouter_secret_key(),
+                base_url=OPENROUTER_API_URL,
+            )
+        )
