dbe2919f7efcbf53952b056fc4771fe5ac579723
joeloverbeck <joeloverbeck@gmail.com>
2024-10-27 13:15:48 +0100

    Fixing possibly empty template files.

--- src/maps/templates_repository.py
+++ src/maps/templates_repository.py
@@ -1,7 +1,12 @@
 from typing import Dict, Optional
 
 from src.base.enums import TemplateType
-from src.filesystem.file_operations import read_json_file, write_json_file
+from src.filesystem.file_operations import (
+    read_json_file,
+    write_json_file,
+    create_directories,
+    create_empty_json_file_if_not_exists,
+)
 from src.filesystem.path_manager import PathManager
 
 
@@ -10,7 +15,13 @@ class TemplatesRepository:
         self._path_manager = path_manager or PathManager()
 
     def load_templates(self, place_type: TemplateType) -> Dict:
+        # Ensure templates dir exists.
+        create_directories(self._path_manager.TEMPLATES_DIR)
+
         file_path = self._path_manager.get_templates_paths(place_type)
+
+        # Ensure the file exists.
+        create_empty_json_file_if_not_exists(file_path)
 
         templates = read_json_file(file_path)
 
