fffd4a9cacd9682ca4e9173b3d437e0664ca2167
joeloverbeck <joeloverbeck@gmail.com>
2024-11-03 23:22:36 +0100

    Ensured that messages don't get overwritten in the ongoing dialogue file.

--- src/dialogues/commands/store_temporary_dialogue_command.py
+++ src/dialogues/commands/store_temporary_dialogue_command.py
@@ -1,9 +1,10 @@
 from typing import Optional
 
 from src.base.abstracts.command import Command
+from src.base.playthrough_manager import PlaythroughManager
 from src.dialogues.participants import Participants
 from src.dialogues.transcription import Transcription
-from src.filesystem.file_operations import write_json_file
+from src.filesystem.file_operations import write_json_file, read_json_file
 from src.filesystem.filesystem_manager import FilesystemManager
 from src.filesystem.path_manager import PathManager
 
@@ -18,13 +19,18 @@ class StoreTemporaryDialogueCommand(Command):
         transcription: Transcription,
         filesystem_manager: Optional[FilesystemManager] = None,
         path_manager: Optional[PathManager] = None,
+        playthrough_manager: Optional[PlaythroughManager] = None,
     ):
         self._playthrough_name = playthrough_name
         self._participants = participants
         self._purpose = purpose
         self._transcription = transcription
+
         self._filesystem_manager = filesystem_manager or FilesystemManager()
         self._path_manager = path_manager or PathManager()
+        self._playthrough_manager = playthrough_manager or PlaythroughManager(
+            self._playthrough_name
+        )
 
     def execute(self) -> None:
         # if at this point we don't have enough participants, we have a problem.
@@ -33,13 +39,25 @@ class StoreTemporaryDialogueCommand(Command):
                 f"About to store the ongoing dialogue, there weren't enough participants. {self._participants.get()}"
             )
 
-        json_data = {
-            "participants": self._participants.get(),
-            "purpose": self._purpose,
-            "transcription": self._transcription.get(),
-        }
+        ongoing_dialogue_file_path = self._path_manager.get_ongoing_dialogue_path(
+            self._playthrough_name
+        )
+
+        # Ensure not to overwrite the messages if there's an existing dialogue.
+        if self._playthrough_manager.has_ongoing_dialogue():
+            ongoing_dialogue_file = read_json_file(ongoing_dialogue_file_path)
+        else:
+            ongoing_dialogue_file = {}
+
+        ongoing_dialogue_file.update(
+            {
+                "participants": self._participants.get(),
+                "purpose": self._purpose,
+                "transcription": self._transcription.get(),
+            }
+        )
 
         write_json_file(
-            self._path_manager.get_ongoing_dialogue_path(self._playthrough_name),
-            json_data,
+            ongoing_dialogue_file_path,
+            ongoing_dialogue_file,
         )
